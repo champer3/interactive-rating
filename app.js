63269b3b0aae56af23fbfe9f9c6d987b23390336
champer3 <okunolas21@gmail.com>
2022-04-12 02:40:12 -0700

    interactive rating

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const submit = document.querySelector('button')
 const container = document.querySelector(".container")
 const thankYou = document.querySelector(".thank-you")
 const rate = document.querySelector(".rate p i")
-console.log(rate)
 
 
 ratings.forEach(rating =>{
-    rating.addEventListener("click", function(){
+    rating.addEventListener("click", function(e){
         rating.classList.toggle('active')
+        rate.innerText = e.target.innerText
     })
 })
 
 submit.addEventListener("click", function(){
     // window.location.href="http:indez1.html"
     container.style.display = "none"
+    thankYou.style.display = "flex"
 })
